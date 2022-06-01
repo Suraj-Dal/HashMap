8cf8f3e58d4cdbe070ec433bcd061f023189c07f
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-02 02:57:17 +0530

    [Suraj]: Usecase 1 to find frequency of the words in a sentence using hash table is added.

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,8 @@
+﻿HashTable.MyMapNode<int, string> hash = new HashTable.MyMapNode<int, string>();
+hash.add(0, "to");
+hash.add(1, "be");
+hash.add(2, "or");
+hash.add(3, "not");
+hash.add(4, "to");
+hash.add(5, "be");
+hash.getFrequency();
